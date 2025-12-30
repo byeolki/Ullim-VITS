8d283c060b308bcf681d7fc8e87da1742c460638
byeolki <byeolki7007@gmail.com>
2025-12-30 21:01:10 +0000

    fix: syntax error

--- ullim_vits/models/discriminator/mpd.py
+++ ullim_vits/models/discriminator/mpd.py
@@ -26,4 +26,39 @@ class DiscriminatorP(nn.Module):
             n_pad = self.period - (t % self.period)
             x = F.pad(x, (0, n_pad), "reflect")
             t = t + n_pad
-        x = x.view(b
+        x = x.view(b, c, t // self.period, self.period)
+
+        for conv in self.convs:
+            x = conv(x)
+            x = F.leaky_relu(x, 0.1)
+            fmap.append(x)
+
+        x = self.conv_post(x)
+        fmap.append(x)
+        x = torch.flatten(x, 1, -1)
+
+        return x, fmap
+
+
+class MultiPeriodDiscriminator(nn.Module):
+    def __init__(self, periods=[2, 3, 5, 7, 11]):
+        super().__init__()
+        self.discriminators = nn.ModuleList([
+            DiscriminatorP(period) for period in periods
+        ])
+
+    def forward(self, y, y_hat):
+        y_d_rs = []
+        y_d_gs = []
+        fmap_rs = []
+        fmap_gs = []
+
+        for d in self.discriminators:
+            y_d_r, fmap_r = d(y)
+            y_d_g, fmap_g = d(y_hat)
+            y_d_rs.append(y_d_r)
+            y_d_gs.append(y_d_g)
+            fmap_rs.append(fmap_r)
+            fmap_gs.append(fmap_g)
+
+        return y_d_rs, y_d_gs, fmap_rs, fmap_gs
--- ullim_vits/models/discriminator/msd.py
+++ ullim_vits/models/discriminator/msd.py
@@ -3,27 +3,64 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class DiscriminatorP(nn.Module):
-    def __init__(self, period, kernel_size=5, stride=3):
+class DiscriminatorS(nn.Module):
+    def __init__(self, use_spectral_norm=False):
         super().__init__()
-        self.period = period
+        norm_f = nn.utils.spectral_norm if use_spectral_norm else nn.utils.weight_norm
 
         self.convs = nn.ModuleList([
-            nn.Conv2d(1, 32, (kernel_size, 1), (stride, 1), padding=(kernel_size//2, 0)),
-            nn.Conv2d(32, 128, (kernel_size, 1), (stride, 1), padding=(kernel_size//2, 0)),
-            nn.Conv2d(128, 512, (kernel_size, 1), (stride, 1), padding=(kernel_size//2, 0)),
-            nn.Conv2d(512, 1024, (kernel_size, 1), (stride, 1), padding=(kernel_size//2, 0)),
-            nn.Conv2d(1024, 1024, (kernel_size, 1), 1, padding=(2, 0)),
+            norm_f(nn.Conv1d(1, 16, 15, 1, padding=7)),
+            norm_f(nn.Conv1d(16, 64, 41, 4, groups=4, padding=20)),
+            norm_f(nn.Conv1d(64, 256, 41, 4, groups=16, padding=20)),
+            norm_f(nn.Conv1d(256, 1024, 41, 4, groups=64, padding=20)),
+            norm_f(nn.Conv1d(1024, 1024, 41, 4, groups=256, padding=20)),
+            norm_f(nn.Conv1d(1024, 1024, 5, 1, padding=2)),
         ])
 
-        self.conv_post = nn.Conv2d(1024, 1, (3, 1), 1, padding=(1, 0))
+        self.conv_post = norm_f(nn.Conv1d(1024, 1, 3, 1, padding=1))
 
     def forward(self, x):
         fmap = []
 
-        b, c, t = x.shape
-        if t % self.period != 0:
-            n_pad = self.period - (t % self.period)
-            x = F.pad(x, (0, n_pad), "reflect")
-            t = t + n_pad
-        x = x.view(b
+        for conv in self.convs:
+            x = conv(x)
+            x = F.leaky_relu(x, 0.1)
+            fmap.append(x)
+
+        x = self.conv_post(x)
+        fmap.append(x)
+        x = torch.flatten(x, 1, -1)
+
+        return x, fmap
+
+
+class MultiScaleDiscriminator(nn.Module):
+    def __init__(self, scales=3):
+        super().__init__()
+        self.discriminators = nn.ModuleList([
+            DiscriminatorS(use_spectral_norm=(i == 0)) for i in range(scales)
+        ])
+
+        self.meanpools = nn.ModuleList([
+            nn.AvgPool1d(4, 2, padding=2) for _ in range(scales - 1)
+        ])
+
+    def forward(self, y, y_hat):
+        y_d_rs = []
+        y_d_gs = []
+        fmap_rs = []
+        fmap_gs = []
+
+        for i, d in enumerate(self.discriminators):
+            if i != 0:
+                y = self.meanpools[i-1](y)
+                y_hat = self.meanpools[i-1](y_hat)
+
+            y_d_r, fmap_r = d(y)
+            y_d_g, fmap_g = d(y_hat)
+            y_d_rs.append(y_d_r)
+            y_d_gs.append(y_d_g)
+            fmap_rs.append(fmap_r)
+            fmap_gs.append(fmap_g)
+
+        return y_d_rs, y_d_gs, fmap_rs, fmap_gs

3a0fcf74f72117c1ab216bf710340f00e1e293ac
byeolki <byeolki7007@gmail.com>
2025-12-31 00:19:22 +0000

    feat: hide nnpack warning logs

--- ullim_vits/cli/train.py
+++ ullim_vits/cli/train.py
@@ -1,7 +1,9 @@
+import warnings
+warnings.filterwarnings('ignore', message='Could not initialize NNPACK') # hate nnpack errors
+
 import hydra
 from omegaconf import DictConfig
 from ullim_vits.training.trainer import Trainer
-
 
 @hydra.main(version_base=None, config_path="../../configs", config_name="config")
 def main(config: DictConfig):
